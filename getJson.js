fd2ebb3ff262ce3f5f157d9c9dde93593218299b
adhokshaja.acharya@mca.christuniversity.in <95070489+Adhokshaja73@users.noreply.github.com>
2022-02-02 02:37:03 +0530

    angular-fault

--- getJson.js
+++ getJson.js
@@ -1,4 +1,7 @@
 var myapp = angular.module("myapp", []);
 myapp.controller("myctrl", function($scope, $http) {
-    $http.get("")
+    $http.get("https://raw.githubusercontent.com/Adhokshaja73/Train-Scheduling-Managment/main/trains.json")
+        .success(function(response) {
+            $scope.names = response.train;
+        })
 });
