e6b3cd82af830d830e661f8f759cc3aa9d715510
adhokshaja.acharya@mca.christuniversity.in <95070489+Adhokshaja73@users.noreply.github.com>
2022-02-11 13:37:38 +0530

    angular-jason-full

--- getJson.js
+++ getJson.js
@@ -3,5 +3,6 @@ myapp.controller("myctrl", function($scope, $http) {
     $http.get("https://raw.githubusercontent.com/Adhokshaja73/Train-Scheduling-Managment/main/trains.json")
         .success(function(response) {
             $scope.names = response.train;
+            console.log($scope.names);
         })
 });
